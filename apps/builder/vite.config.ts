93d4965b2e881862d613fe825ae900565d07608c
Duouii <2890951490@qq.com>
2025-11-18 00:23:44 +0800

    style: 💄  format code

--- apps/builder/vite.config.ts
+++ apps/builder/vite.config.ts
@@ -3,41 +3,44 @@
 // @ts-ignore
 import { visualizer } from 'rollup-plugin-visualizer'
 import veauryVitePlugins from 'veaury/vite/index.js'
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig({
-    base: '/xc-vbuilder/',
-    plugins: [
-        /*vue(), vueJsx(),*/ vueDevTools(),
-        veauryVitePlugins({ type: 'vue' }),
-        visualizer({ open: true })
-    ],
-    resolve: {
-        alias: {
-            '@': '/src'
-        }
-    },
-    server: {
-        port: 3000,
-        proxy: {
-            '/charts': {
-                target: 'https://echarts.apache.org',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/charts/, '')
+export default defineConfig(({ command }: ConfigEnv) => {
+    const isDev = command === 'serve'
+    return {
+        base: isDev ? '/' : '/xc-vbuilder/',
+        plugins: [
+            /*vue(), vueJsx(),*/ vueDevTools(),
+            veauryVitePlugins({ type: 'vue' }),
+            visualizer({ open: true })
+        ],
+        resolve: {
+            alias: {
+                '@': '/src'
             }
-        }
-    },
-    build: {
-        sourcemap: true,
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (/(echarts|d3|zrender)/.test(id)) {
-                            return 'charts'
+        },
+        server: {
+            port: 3000,
+            proxy: {
+                '/charts': {
+                    target: 'https://echarts.apache.org',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/charts/, '')
+                }
+            }
+        },
+        build: {
+            sourcemap: true,
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            if (/(echarts|d3|zrender)/.test(id)) {
+                                return 'charts'
+                            }
+                            return 'vendor'
                         }
-                        return 'vendor'
                     }
                 }
             }
